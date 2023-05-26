997969d50f5e01007f07962656daad3795716dcf
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-05-26 19:48:50 -0300

    Terminar

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace AlcoolOuGasosa;
+class Program
+{
+    static void Main(string[] args)
+    {
+        decimal gasosa;
+        decimal Álcool;
+        
+        Console.Clear();
+        
+        Console.WriteLine("Álcool ou Gasosa?");
+
+        Console.WriteLine();
+
+        Console.Write("Digite o preço da gasosa...: R$ ");
+        gasosa = Convert.ToDecimal(Console.ReadLine());
+
+
+        Console.Write("Digite o preço do Álcool...: R$ ");
+        Álcool = Convert.ToDecimal(Console.ReadLine());
+    }
+}
